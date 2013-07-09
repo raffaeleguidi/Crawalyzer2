3f3f1adaf7c41967ea2d70c67381d9a4e6d14674
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2013-07-09 23:48:43 +0200

    initial commit

---
+++ project/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "crawalyzer2"
+  val appVersion      = "1.0-SNAPSHOT"
+
+	val appDependencies = Seq(
+	  "org.reactivemongo" %% "play2-reactivemongo" % "0.9",
+    "org.jsoup" % "jsoup" % "1.7.2"
+	)
+	
+	val main = play.Project(appName, appVersion, appDependencies).settings(
+	  //resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+	)
+
+}
